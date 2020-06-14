b7f10cafa38067763418b3739306af9371125749
Alexa Grey <devel@mouse.vc>
2020-06-14 14:48:31 -0700

    Add github workflow for testing

--- db/connection.rb
+++ db/connection.rb
@@ -4,6 +4,7 @@ class DBConnector
   attr_reader :options
   attr_reader :rails_version
   attr_reader :type
+  attr_reader :url
 
   def initialize
     @options = {}
@@ -12,6 +13,10 @@ class DBConnector
 
     @gemfile = File.basename(ENV["BUNDLE_GEMFILE"] || "", ".gemfile")
 
+    @url = ENV["DATABASE_URL"]
+
+    @options[:url] = @url if @url.present?
+
     _, @rails_version, @type = *@gemfile.match(/\Arails_(\d+)_(.+)\z/)
 
     case @type
@@ -19,11 +24,11 @@ class DBConnector
       options[:adapter]   = "mysql2"
       options[:username]  = ENV.fetch("MYSQL_USER", "root")
       options[:password]  = ENV.fetch("MYSQL_PASS", nil)
-      options[:database]  = "wrapped_transaction_test"
+      options[:database]  = ENV.fetch("DB_NAME", "wrapped_transaction_test")
       options[:encoding]  = "utf8"
     when /pg\z/
       options[:adapter]   = "postgresql"
-      options[:database]  = "wrapped_transaction_test"
+      options[:database]  = ENV.fetch("DB_NAME", "wrapped_transaction_test")
     when /sqlite3\z/
       options[:adapter]   = "sqlite3"
       options[:database]  = File.join(__dir__, "wrapped_transaction_test.sqlite3")
@@ -39,7 +44,7 @@ end
 
 DBENV = DBConnector.new
 
-ActiveRecord::Base.establish_connection DBENV.options
+ActiveRecord::Base.establish_connection DBENV.url || DBENV.options
 
 require_relative "./schema"
 require_relative "./models"
